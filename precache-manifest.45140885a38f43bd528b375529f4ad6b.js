4e3e11d67ff988d0d338c25d6e4ac35d2aee6c1e
sekhart <sekhartsr@gmail.com>
2020-01-06 12:02:37 +0800

    Updates

---
+++ precache-manifest.45140885a38f43bd528b375529f4ad6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5f736ad9795b5626dbb895c1c8ebd0",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "bacd9e46060cec9af882",
+    "url": "/react-app/static/css/main.b8765e77.chunk.css"
+  },
+  {
+    "revision": "c677d852ee211ca6a241",
+    "url": "/react-app/static/js/2.fd8f21d5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.fd8f21d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bacd9e46060cec9af882",
+    "url": "/react-app/static/js/main.bbead4fe.chunk.js"
+  },
+  {
+    "revision": "6b7c356b973b13ce0aac",
+    "url": "/react-app/static/js/runtime-main.d8989379.js"
+  }
+]);
