0d6a4dcb9e9dd4f3be67c00ca28de6877d460a56
sekhart <sekhartsr@gmail.com>
2020-01-06 12:00:11 +0800

    first commit

---
+++ src/App1.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import Table from './Table.js'
+import Form from './Form.js'
+
+class App extends Component {
+    state = {
+        characters: [],
+    }
+    render() {
+        const { characters } = this.state
+
+        return (
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        )
+    }
+    removeCharacter = index => {
+        const { characters } = this.state
+      
+        this.setState({
+          characters: characters.filter((character, i) => {
+            return i !== index
+          }),
+        })
+      }
+
+     handleSubmit = character => {
+        this.setState({ characters: [...this.state.characters, character] })
+      }
+}
+
+export default App
